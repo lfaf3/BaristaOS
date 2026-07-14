480194c1cd62e647e0929ee7e72fffffec66d5df
lfaf3 <lfelyphe@gmail.com>
2026-07-14 15:59:57 +0000

    teste

---
+++ src/components/CafeBrand.tsx
@@ -0,0 +1,17 @@
+export function CafeBrand() {
+  return (
+    <div className="cafe-brand">
+      <div className="cafe-brand__logo-wrapper">
+        <img
+          className="cafe-brand__logo"
+          src="/brands/dm-caffe-sidebar-logo.png"
+          alt="DM Caffè"
+        />
+      </div>
+
+      <span className="cafe-brand__powered">
+        BaristaOS
+      </span>
+    </div>
+  );
+}

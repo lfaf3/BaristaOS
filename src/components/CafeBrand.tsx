70f64221938325f72fe6617432e4e9c52415afc3
lfaf3 <lfelyphe@gmail.com>
2026-07-14 16:03:02 +0000

    teste

--- src/components/CafeBrand.tsx
+++ src/components/CafeBrand.tsx
@@ -4,7 +4,7 @@ export function CafeBrand() {
       <div className="cafe-brand__logo-wrapper">
         <img
           className="cafe-brand__logo"
-          src="/brands/dm-caffe-sidebar-logo.png"
+          src="/public/brands/dm-caffe-sidebar-logo.png"
           alt="DM Caffè"
         />
       </div>
